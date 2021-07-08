9650af012991ceb644edf085fc88eefa87d0858b
light4everyone <jenea94@gmail.com>
2021-07-08 19:18:20 +0300

    Update .net to .net 5.0
    Add Swagger

--- src/OnlineBookShop.Api/OnlineBookShop.API/Startup.cs
+++ src/OnlineBookShop.Api/OnlineBookShop.API/Startup.cs
@@ -47,6 +47,8 @@ namespace OnlineBookShop.API
 
             services.AddScoped<IRepository, EFCoreRepository>();
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,6 +62,13 @@ namespace OnlineBookShop.API
             {
                 app.UseMiddleware<ErrorHandlingMiddleware>();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Online Book Shop Api");
+            });
 
             app.UseRouting();
 
--- src/OnlineBookShop.React/src/config.ts
+++ src/OnlineBookShop.React/src/config.ts
@@ -1,3 +1,5 @@
-export default {
+const config = {
   API_URL: process.env.REACT_APP_API_URL
 }
+
+export default config;
